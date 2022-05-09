445053a78fbed86b1bcc90280c109fe1e965ae26
humminghorse <imahashi@guildworks.jp>
2022-05-10 08:39:21 +0900

    1 binaries uploaded

---
+++ components/CoordinationListItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="card w-1/3 bg-base-100 h-auto mt-2 shadow-xl h-64">
+    <figure class="px-2 pt-2">
+      <img
+        src="@/assets/image/miru-red-collar.jpg"
+        alt="Miru"
+        class="rounded-xl"
+      />
+    </figure>
+    <div class="card-body items-center text-center">
+      <!-- <h2 class="card-title -mt-6">みる <span class="-m-2 text-sm">ちゃん</span></h2> -->
+      <h3 class="text-sm -ml-24 -mt-4">着用中のアイテム</h3>
+      <div class="flex items-center">
+        <img
+          src="~/assets/image/catlog-red.jpg"
+          alt="Catlog Red"
+          class="rounded-xl h-12"
+        />
+        <p class="text-sm ml-4">Catlog<br />(カラー: Red)</p>
+      </div>
+      <div class="card-actions mt-2">
+        <button class="btn btn-primary w-full">
+          アイテムの<br />詳細を見る
+        </button>
+        <button class="btn btn-primary w-full">
+          この子の<br />他のコーディネートを見る
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
