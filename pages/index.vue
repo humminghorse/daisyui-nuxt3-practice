2f66b3b3d388746117283e9b685de2bdfa8e2f56
humminghorse <imahashi@guildworks.jp>
2022-05-10 09:23:30 +0900

    Radioコンポーネント作成

--- pages/index.vue
+++ pages/index.vue
@@ -17,142 +17,26 @@
     <!-- body -->
     <div class="flex bg-neutral">
       <div class="control-panel bg-white flex-auto m-4 p-2 w-1/12">
-        <h3 class="text-lg mt-2">ペットの種類</h3>
-        <div class="form-control">
-          <label class="label cursor-pointer justify-start">
-            <input
-              type="radio"
-              name="radio-pet-type"
-              class="radio checked:bg-accent mr-2"
-              checked
-            />
-            <span class="label-text">犬</span>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer justify-start">
-            <input
-              type="radio"
-              name="radio-pet-type"
-              class="radio checked:bg-accent mr-2"
-              checked
-            />
-            <span class="label-text">猫</span>
-          </label>
-        </div>
-        <h3 class="text-lg mt-2">ペットの色</h3>
-        <div class="form-control">
-          <label class="label cursor-pointer justify-start">
-            <input
-              type="radio"
-              name="radio-pet-color"
-              class="radio checked:bg-accent mr-2"
-              checked
-            />
-            <span class="label-text">白</span>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer justify-start">
-            <input
-              type="radio"
-              name="radio-pet-color"
-              class="radio checked:bg-accent mr-2"
-              checked
-            />
-            <span class="label-text">黒</span>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer justify-start">
-            <input
-              type="radio"
-              name="radio-pet-color"
-              class="radio checked:bg-accent mr-2"
-              checked
-            />
-            <span class="label-text">茶</span>
-          </label>
-        </div>
-        <h3 class="text-lg mt-2">アイテムの種類</h3>
-        <div class="form-control">
-          <label class="label cursor-pointer justify-start">
-            <input
-              type="radio"
-              name="radio-item-type"
-              class="radio checked:bg-accent mr-2"
-              checked
-            />
-            <span class="label-text">首輪</span>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer justify-start">
-            <input
-              type="radio"
-              name="radio-item-type"
-              class="radio checked:bg-accent mr-2"
-              checked
-            />
-            <span class="label-text">服</span>
-          </label>
-        </div>
-        <h3 class="text-lg mt-2">アイテムの色</h3>
-        <div class="form-control">
-          <label class="label cursor-pointer justify-start">
-            <input
-              type="radio"
-              name="radio-item-color"
-              class="radio checked:bg-accent mr-2"
-              checked
-            />
-            <span class="label-text">白</span>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer justify-start">
-            <input
-              type="radio"
-              name="radio-item-color"
-              class="radio checked:bg-accent mr-2"
-              checked
-            />
-            <span class="label-text">黒</span>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer justify-start">
-            <input
-              type="radio"
-              name="radio-item-color"
-              class="radio checked:bg-accent mr-2"
-              checked
-            />
-            <span class="label-text">赤</span>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer justify-start">
-            <input
-              type="radio"
-              name="radio-item-color"
-              class="radio checked:bg-accent mr-2"
-              checked
-            />
-            <span class="label-text">青</span>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer justify-start">
-            <input
-              type="radio"
-              name="radio-item-color"
-              class="radio checked:bg-accent mr-2"
-              checked
-            />
-            <span class="label-text">黄</span>
-          </label>
-        </div>
+        <Radio
+          radioTitle="ペットの種類"
+          radioName="radio-pet-type"
+          v-bind:labelTexts="['犬', '猫']"
+        />
+        <Radio
+          radioTitle="ペットの色"
+          radioName="radio-pet-color"
+          v-bind:labelTexts="['白', '黒', '茶']"
+        />
+        <Radio
+          radioTitle="アイテムの種類"
+          radioName="radio-item-type"
+          v-bind:labelTexts="['首輪', '服']"
+        />
+        <Radio
+          radioTitle="アイテムの色"
+          radioName="radio-item-color"
+          v-bind:labelTexts="['白', '黒', '赤', '青', '黄色']"
+        />
       </div>
       <div class="content flex flex-wrap mx-4 my-4 w-2/3">
         <template v-for="n of 5" :key="n">
@@ -160,14 +44,15 @@
         </template>
       </div>
     </div>
+
     <TheFooter />
   </div>
 </template>
 <style lang="css">
-.bg-orange-sample {
+/* .bg-orange-sample {
   background-color: #e7541c;
 }
 .bg-blue-sample {
   background-color: #000099;
-}
+} */
 </style>
